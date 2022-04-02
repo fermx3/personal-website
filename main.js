26691722ef6b0494e201a461770912b12f29a3fd
fermx3 <99512170+fermx3@users.noreply.github.com>
2022-04-02 10:51:22 -0600

    Small buttons now change color in dark theme

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
+//Dark Theme Switch
 
-//Dark Theme
-
-$( "#dark-mode" ).click(function() {
-  $("#button" ).toggleClass("fa-toggle-on fa-toggle-off")
-  $( "nav" ).toggleClass("bg-dark")
-  $( "nav" ).toggleClass("navbar-light navbar-dark");
-  $( "body" ).toggleClass("dark-background");
-  $( ".white-section" ).toggleClass("bg-light bg-dark");
-  $( ".border" ).toggleClass("border");
+$("#dark-mode").click(function() {
+  $("#button").toggleClass("fa-toggle-on fa-toggle-off")
+  $("nav").toggleClass("bg-dark")
+  $("nav").toggleClass("navbar-light navbar-dark");
+  $("body").toggleClass("dark-background");
+  $(".white-section").toggleClass("bg-light bg-dark");
+  $(".border").toggleClass("border");
+  $(".btn-outline-dark").toggleClass("btn-outline-dark btn-outline-light");
 });
