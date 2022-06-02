f5d3aabcdf38d3893ab5aef923f46f49309d0bc5
Fernando Manuel Rios Alcantara <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2022-06-02 13:32:58 -0500

    Delete unused files

--- themePreference.js
+++
@@ -1,5 +0,0 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    darkModeToggle();
-    checkbox.classList.add("on");
-    checkbox.setAttribute("aria-checked", "true");
-  }
